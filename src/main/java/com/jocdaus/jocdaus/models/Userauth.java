07e95aaaa45a01a3ac92a6d709a2e130dd1ab4c1
joanformacio <joanlmformacio@putlook.es>
2021-05-26 09:25:33 +0200

    Authentication

--- src/main/java/com/jocdaus/jocdaus/models/Userauth.java
+++ src/main/java/com/jocdaus/jocdaus/models/Userauth.java
@@ -5,28 +5,30 @@
  */
 package com.jocdaus.jocdaus.models;
 
-import com.jocdaus.jocdaus.rolenum.Roles;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 
-/**
- *
- * @author Joan
- */
-public class Userauth extends User{
-    private Roles role;
+public class Userauth {
+    private String username;
+    private String password;
     private String token;
     
-    public Userauth(String username, String password){
-        super (username, password);
+    
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
    
-
     public String getToken() {
         return token;
     }
@@ -34,27 +36,5 @@ public class Userauth extends User{
     public void setToken(String token) {
         this.token = token;
     }
-    
-private String getJWTToken(String username){
-    String secretKey="mySecrtKey";
-    List<GrantedAuthority> grantedAuthorities=AuthorityUtils
-            .commaSeparatedStringToAuthorityList("ROLE_USER");
-    
-    String token = Jwts
-            .builder()
-            .setId("softtekJWT")
-            .setSubject(username)
-            .claim("autorities",
-                    grantedAuthorities.stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .collect(Collectors.toList()))
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + 600000))
-            .signWith(SignatureAlgorithm.HS512,
-                    secretKey.getBytes()).compact();
-    
-    return "Bearer" + token;
-}
-    
-   
+  
 }
